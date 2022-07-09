0e88cec6540b319621d46ee83efdafebc243ab61
WolfWaddle <97347242+WolfWaddle@users.noreply.github.com>
2022-07-08 19:37:27 -0500

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,12 +1,7 @@
+I
 import fs from 'fs'
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let text = `
-*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙱𝙾𝚇𝙼𝙸𝙽𝙴 𝙷𝙾𝚂𝚃*
-> Tutorial: https://youtu.be/eC9TfKICpcY
-> Pagina Oficial: https://boxmineworld.com
-> Dashboard: https://dash.boxmineworld.com/home
-> Panel: https://panel.boxmineworld.com
-
 ------------------------------------
 
 *—◉ 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚃𝙴𝚁𝙼𝚄𝚇*
@@ -19,7 +14,7 @@ let text = `
 > pkg install ffmpeg -y
 > pkg install imagemagick -y
 > pkg install yarn
-> git clone https://github.com/WolfWaddle/TheMystic-Bot-MD
+> git clone https://github.com/WolfWaddle/TheWolf-Bot-MD-V2
 > cd TheMystic-Bot-MD
 > npm install
 > yarn install 
@@ -32,7 +27,7 @@ contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: nul
 title: '𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽 - 𝙸𝙽𝚂𝚃𝙰𝙻𝙰𝚁𝙱𝙾𝚃',
 body: 'ᴛʜᴇ ᴍʏsᴛᴄ ﹣ ʙᴏᴛ',         
 previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `https://github.com/WolfWaddle/TheMystic-Bot-MD`}}})   
+sourceUrl: `https://github.com/WolfWaddle/TheWolf-Bot-MD-V2`}}})   
 }
 handler.command = /^(instalarbot)/i
 export default handler
