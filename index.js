389d740e59ac0ee4f8bdc7689f21ad808fca2c03
WolfWaddle <97347242+WolfWaddle@users.noreply.github.com>
2022-07-19 16:54:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('TheWolf-Whatsapp-Bot-MD-V2', {
+say('Wolf-Whatsapp-Bot-MD-V2', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
